6c73257bb37b2a92d18ff4ea21b3c1b3b57d08be
fegger <f.egger@awmc.uq.edu.au>
2017-11-07 13:12:21 +1000

    Initial Commit
    Initial Commit of parameter estimation scripts

--- optimSulfide.m
+++
@@ -1,7 +0,0 @@
-function f=optimSulfide(p,tspan,ySys,s,yIni)
-
-options = odeset('RelTol',1e-4,'Stats','on','OutputFcn',@odeplot);
-[t,y,p]=ode15s(@sulfideKinetics,tspan,yIni,options,p);
-
-f=sqrt(abs(1./s)).*(y(:,1:2)-ySys);
-
